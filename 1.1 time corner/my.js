1a6849b12e8c0ed5c122bec47d994fe37ad8e0b7
Dima Esipenko <mif83@mail.ru>
2016-12-26 10:44:20 +0200

    complete

--- 1.1 time corner/my.js
+++ 1.1 time corner/my.js
@@ -10,16 +10,23 @@ function calcCorner(str){
         minuteArrowCorner,
         hourArrowCorner,
         diffCorner;
-    if (time[0] >= 24 || time[0] <= 0 || time[1]>= 59 || time[1] <= 0 ) return;
-    if (time[0]>=12) time[0] = time[0]/12;
+    if (time[0] > 24 || time[0] < 0 || time[1]>= 59 || time[1] < 0 ) return;
+    if (time[0] == 24){
+        time[0] = 0;
+    };
+    if (time[0]>=12) {
+        time[0] = time[0]-12;
+    } ;
+    //одна минута это 360/60 = 6 градусов
     minuteArrowCorner = time[1]*6;
-
-    hourArrowCorner = (+time[0] + Math.round(time[1]*5/60))*6;
+    // учитываем отклонение часовой стрелки, 5 делений минут между каждым часом
+    hourArrowCorner = time[0]*6*5 + Math.round(time[1]*5/60);
     diffCorner = Math.abs(minuteArrowCorner - hourArrowCorner);
-    return diffCorner;
+
+    return diffCorner > 180 ? Math.abs(360- diffCorner) : diffCorner ;
 };
 function showCorner(str){
-    document.getElementById("result").innerHTML = "Результат = " + str;
+    document.getElementById("result").innerHTML = "Результат = " + str + "&deg;";
 };
 function run(){
     console.log("run");
