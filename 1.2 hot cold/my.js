b7962e5de7f7c4a5fdf15cd98fbadffd640bf0f8
Dima Esipenko <mif83@mail.ru>
2016-12-27 14:10:05 +0200

    1.2 task 100% ready

--- 1.2 hot cold/my.js
+++ 1.2 hot cold/my.js
@@ -1,32 +1,62 @@
 /**
  * Created by dima on 26.12.2016.
  */
-function getRandomInt(min, max){
-    return Math.floor(Math.random() * (max + 1 - min)) + min;
-};
-function chekNumber(num){
-    if (!last && num !== last) return num + " не угадали"
-    if ( Math.abs(digit - last ) >= Math.abs(digit - num) ){
-        showResult(true, num);
-    } else {
-        showResult(false, num);
-    }
-
-}
-function showResult(hot, num){
-    var result = document.getElementById("result"),
-        li = document.createElement("li"),
-        text;
-
-    if (hot) {
-        text = document.createTextNode(num + " горячо");
-    } else {
-        text = document.createTextNode(num + " холодно");
-    };
-    li.appendChild(text);
-    result.appendChild(li);
-}
 var digit = getRandomInt(0, 100),
-    last;
+    last,
+    try1 = 10,
+    win = false;
 
-// document.getElementById("input-digit").value
+document.getElementById("run").addEventListener("click", run);
+document.getElementById("reset").addEventListener("click", reset);
+
+function chekResult(userValue){
+    var str = "";
+    if (userValue == digit) {
+        str =  "вы угадали";
+        win = true;
+    } else if (!last){
+        str =  userValue + " первый промах";
+    } else if (last > Math.abs(userValue - digit) ){
+        str =  userValue + " теплее";
+    } else{
+        str =  userValue + " холоднее";
+    };
+    last = Math.abs(userValue - digit);
+    try1--;
+    return str;
+
+};
+function showResult(str){
+    var ul = document.getElementById("result"),
+        li = document.createElement("li"),
+        text = document.createTextNode(str);
+    li.appendChild(text);
+    ul.appendChild(li);
+    document.querySelector("h1 span").innerHTML = try1;
+};
+function clear(){
+    try1 = 10;
+    win = false;
+    last = undefined;
+    document.querySelector("h1 span").innerHTML = try1;
+    document.getElementById("result").innerHTML = "";
+};
+function run(){
+    var value = +document.getElementById("input-digit").value,
+        str;
+    if (win){
+        return;
+    }
+    if ( try1 > 0){
+        str = chekResult(value);
+    } else {
+        str = "попытки закончились"
+        return;
+    };
+    showResult(str);
+};
+function reset(){
+    digit = getRandomInt(0, 100);
+    clear();
+};
+
