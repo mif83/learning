3d8a8ffd67f9bedf6905c65724769496629bf1d5
Dima Esipenko <mif83@mail.ru>
2017-01-15 15:45:12 +0200

    1.3 canvas buble  - draws a circle by clicking

--- 1.3 canvas buble/my.js
+++ 1.3 canvas buble/my.js
@@ -1,10 +1,54 @@
 /**
  * Created by dima on 12.01.2017.
  */
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
+var canvas = document.getElementById("canvas"),
+    ctx = canvas.getContext("2d"),
+    count = 10,
+    arrCircles = [];
 
-ctx.strokeStyle = '#00FF00'; // меняем цвет рамки
-ctx.strokeRect(125, 15, 100, 100);
-ctx.fillStyle = '#5500FF'; // меняем цвет прямоугольника
-ctx.fillRect(130, 20, 90, 90);
+
+
+
+
+canvas.addEventListener("click", getCircle);
+
+
+function getCircle(e) {
+    if (count <= 0 ) return; // можем рисовать столько окружностей сколько задано в count
+    arrCircles.push(getCursorPosition(e));
+    drawCircle();
+    count--;
+
+}
+function moveCircle(obj) {
+    canvas.width = canvas.width;
+
+}
+
+function drawCircle(){
+    for (var i = 0; i < arrCircles.length; i++) {
+        ctx.beginPath();
+        ctx.arc(arrCircles[i].x, arrCircles[i].y, 30, 0, Math.PI * 2);
+        ctx.fillStyle = "#69cc27";
+        ctx.fill();
+    }
+}
+
+// получаем координаты клика в элементе canvas
+function getCursorPosition(e) {
+    var obj ={};
+
+    if (e.pageX != undefined && e.pageY != undefined) {
+        obj.x = e.pageX;
+        obj.y = e.pageY;
+    }
+    else {
+        obj.x = e.clientX + document.body.scrollLeft +
+            document.documentElement.scrollLeft;
+        obj.y = e.clientY + document.body.scrollTop +
+            document.documentElement.scrollTop;
+    }
+    obj.x -= canvas.offsetLeft;
+    obj.y -= canvas.offsetTop;
+    return obj;
+}
